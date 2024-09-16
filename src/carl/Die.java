3b225097cc787de14d59703e5cf4574442fe3308
CarlShow <carl.show@yahoo.com>
2024-09-16 13:05:53 -0500

    Giving StyleGuide another gander; maybe it'll be a home run, maybe it'll do about nothing.

--- src/carl/Die.java
+++ src/carl/Die.java
@@ -9,20 +9,39 @@ package carl;
 
 import java.util.Random;
 
+/**
+ *   A modular die that has to be rolled prior to knowing its output.
+ *   Its sides may be a number between 2 and 100.
+ */
 public class Die {
     int sides;
     int currentValue;
     Random roller = new Random();
+
+    /**
+     *   Makes the die
+     *   @param sides The number of sides on the die; maxed out at 100.
+     */
     public Die(int sides) {
-        if(sides <= 100 && sides >= 2) {
+        if(sides <= ((((3*4)+3)*3)*2+3+2) && sides >= 2) { // Skating around magic numbers is tough
             this.sides = sides;
         } else {
             throw new IllegalArgumentException("Sides must be between 2 and 100 inclusive");
         }
     }
+
+    /**
+     *   Rolls the die, giving it a random value.
+     */
     public void roll() {
         currentValue = (Math.abs(roller.nextInt()) % sides + 1) + 1;
     }
+
+    /**
+     *   Returns the die's current face value
+     *   @return int
+     *   @throws DieNotRolledException If the die was yet to roll, throw an exception.
+     */
     public int getValue() throws DieNotRolledException {
         try {
             return currentValue;
--- src/carl/Driver.java
+++ src/carl/Driver.java
@@ -6,23 +6,42 @@
  * Last Updated: September 12, 2024
  */
 package carl;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ *   The driver class, instances the instantiate dice method
+ *   and catches any errors it may throw.
+ */
 public class Driver {
     public static void main(String[] args) {
-        try {
-            instantiateDice();
-        } catch (IllegalArgumentException e) {
-            instantiateDice();
-        }
+        boolean didComplete = false;
+        do {
+            try {
+                didComplete = instantiateDice();
+            } catch (IllegalArgumentException e) {
+                System.out.println("Dice failed to be made: " + e.getMessage());
+            } catch (NegativeArraySizeException e) {
+                System.out.println("Dice failed to roll: Bad number of dice");
+            }
+        } while (!didComplete);
     }
-    public static void instantiateDice() {
+
+    /**
+     *   This is the parent method invokes all the other methods required to create the dice
+     *   @return Returns true once completed without error
+     */
+    public static boolean instantiateDice() {
         int[] temp = getInput();
         Die dice = createDice(temp[1]);
         rollDice(dice, temp[0], temp[2]);
+        return true;
     }
+
+    /**
+     *   Gathers user input and outputs it as an int[]
+     *   @return int[], with [0] as the # of dice, [1] as the sides per die, and [2]
+     *   as the number of rolls
+     */
     public static int[] getInput() {
         Scanner scan = new Scanner(System.in);
         int[] output = new int[3];
@@ -56,9 +75,24 @@ public class Driver {
         }
         return output;
     }
+
+    /**
+     *   A small method that makes the die
+     *   @param sides The number of sides on the die
+     *   @return The created die
+     *   @throws IllegalArgumentException If an illegal amount of sides is given, it
+     *   spits back an exception
+     */
     public static Die createDice(int sides) throws IllegalArgumentException {
         return new Die(sides);
     }
+
+    /**
+     *   The final major method, this function rolls the dice based on input
+     *   @param dice The created die to be rolled
+     *   @param numDice The number of dice to be rolled
+     *   @param rolls The times this set of dice should be rolled
+     */
     public static void rollDice(Die dice, int numDice, int rolls) {
         int calcMax = numDice*dice.sides;
         int[] outputCount = new int[calcMax + 1];
@@ -81,14 +115,19 @@ public class Driver {
             perUnit = Math.floor(perUnit);
         }
         for (int i = numDice; i < outputCount.length; i++) {
-            String workingString = "";
-            workingString += i + "\t: " + outputCount[i] + "\t";
-            for(int j = 0; j < outputCount[i]/perUnit; j++) {
+            String workingString = i + "\t: " + outputCount[i] + "\t || ";
+            for(int j = 0; j < Math.floor(outputCount[i]/perUnit); j++) {
                 workingString += "*";
             }
             System.out.println(workingString);
         }
     }
+
+    /**
+     *   Finds the maximum within an int[]
+     *   @param input The int[] to be searched
+     *   @return int, the maximum.
+     */
     public static int findMax(int[] input) {
         int indexOfMax = 0;
         int max = 0;
